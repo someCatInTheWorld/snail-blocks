82402bfeb72824a1dca95e8736b93d2366b0d3a2
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2024-04-18 21:16:31 -0600

    Revert "bleh"
    This reverts commit f43bba7c3a49c0d84152b6704fd699152a51ac36.

--- core/block.js
+++ core/block.js
@@ -1519,19 +1519,18 @@ Blockly.Block.prototype.appendInput_ = function(type, name, opt_defaultBlock) {
   if (type == Blockly.INPUT_VALUE || type == Blockly.NEXT_STATEMENT) {
     connection = this.makeConnection_(type);
   }
-  /*
   if (opt_defaultBlock) {
     var newBlock = this.workspace.newBlock(opt_defaultBlock.type)
-    newBlock.setShadow(true)
     newBlock.initSvg()
     if (typeof opt_defaultBlock.value !== 'undefined') {
       try {
         newBlock.setFieldValue(opt_defaultBlock.value, opt_defaultBlock.fieldName)
       } catch (err) {}
     }
+    newBlock.setShadow(true)
     newBlock.outputConnection.connect(connection)
     newBlock.render()
-  }*/
+  }
   var input = new Blockly.Input(type, name, this, connection);
   // Append input to list.
   this.inputList.push(input);
